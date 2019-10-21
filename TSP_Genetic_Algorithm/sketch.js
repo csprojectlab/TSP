bd6e9dd7c98845861621bdf10ddbd9a955121ed6
Aridaman Nandan <aridaman1995@gmail.com>
2019-10-21 16:44:36 +0530

    added the fitness function

--- TSP_Genetic_Algorithm/sketch.js
+++ TSP_Genetic_Algorithm/sketch.js
@@ -11,8 +11,7 @@ let cities = [];
 let recordDistane;
 let bestRoute;
 let population = [];
-
-
+let fitness = [];
 
 function setup () {
     createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -25,10 +24,25 @@ function setup () {
     for (let i = 0; i < POPULATION_COUNT; i++) {
         population[i] = shuffle(order);
     }
-    
+    for (let i = 0; i < POPULATION_COUNT; i++) {
+        // Find distance of this particular order of cities
+        let distance = calculateDistance(cities, population[i]);
+        fitness[i] = distance;     // It should be inverse. 
+    }    
 }
 
 
 function draw () {
    
+}
+
+function calculateDistance (points, mapping) {
+    let sum = 0;
+    for (let i = 0; i < mapping.length - 1; i++) {
+        let cityA = points[mapping[i]];
+        let cityB = points[mapping[i+1]];
+        let distance = dist (cityA.x, cityA.y, cityB.x, cityB.y);
+        sum += distance;
+    }
+    return sum;
 }
